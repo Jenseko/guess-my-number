cafc20567f2ab4817bc26e2bebe2a368f25def38
Jenseko <jenscode23@gmail.com>
2023-10-05 00:22:47 +0200

    add score counting

--- script.js
+++ script.js
@@ -9,6 +9,26 @@
 // document.querySelector('.guess').textContent = 23;
 // console.log(document.querySelector('.guess').value);
 
+// Math.trunc --> schneidet Dezimalstellen ab
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', () => {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = ' ⛔️ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🔥 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = ' 📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
 });
