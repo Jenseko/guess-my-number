b53cedc7e302a913fbc93321b7672d072bfd4d48
Jenseko <jenscode23@gmail.com>
2023-10-10 10:27:46 +0200

    implementing highscore

--- script.js
+++ script.js
@@ -12,6 +12,7 @@
 // Math.trunc --> schneidet Dezimalstellen ab
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +27,11 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // wenn Input zu hoch ist
   } else if (guess > secretNumber) {
